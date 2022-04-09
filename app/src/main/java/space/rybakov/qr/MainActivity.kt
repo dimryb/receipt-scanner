122ed72cc0e083d8eebd4c9f7b6a1e50bf20788f
Dmitriy <dimryb@bk.ru>
2022-04-10 00:09:20 +0700

    Улучшенный вариант получения разрешения

--- app/src/main/java/space/rybakov/qr/MainActivity.kt
+++ app/src/main/java/space/rybakov/qr/MainActivity.kt
@@ -7,8 +7,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.zxing.WriterException
@@ -19,8 +22,9 @@ class MainActivity : AppCompatActivity() {
     var bGenerate: Button? = null
     var bScanner: Button? = null
     var textView: TextView? = null
-    var counter : Int = 0
+    var counter: Int = 0
     var scannerIntent: Intent? = null
+    private lateinit var pLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +32,16 @@ class MainActivity : AppCompatActivity() {
         im = findViewById(R.id.imageView)
         bGenerate = findViewById(R.id.button)
         bScanner = findViewById(R.id.bScan)
-        bScanner?.setOnClickListener{
-            checkCameraPermission()
-        }
         bGenerate?.setOnClickListener{
             generateQrCode("папуличка любимый! $counter")
             ++counter
         }
         textView = findViewById(R.id.textView)
         textView?.text = "Текст сканера"
+        registerPermissionListener()
+        bScanner?.setOnClickListener{
+            checkCameraPermission()
+        }
     }
 
     private fun generateQrCode(text: String){
@@ -50,29 +55,54 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkCameraPermission(){
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 12)
-        }else{
-            scannerIntent = Intent(this, ScannerActivity::class.java)
-            startActivity(scannerIntent)
+        when{
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(this, "Camera run", Toast.LENGTH_LONG).show()
+                    scannerIntent = Intent(this, ScannerActivity::class.java)
+                    startActivity(scannerIntent)
+                }
+
+            shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA) -> {
+                Toast.makeText(this, "We need your permission", Toast.LENGTH_LONG).show()
+            }
+            else -> {
+                pLauncher.launch(android.Manifest.permission.CAMERA)
+            }
         }
+//        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+//            != PackageManager.PERMISSION_GRANTED){
+//            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 12)
+//        }else{
+//            scannerIntent = Intent(this, ScannerActivity::class.java)
+//            startActivity(scannerIntent)
+//        }
     }
 
     /**
      * Вызовется при установке разрешения
      */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//
+//        if (requestCode == 12){
+//            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                scannerIntent = Intent(this, ScannerActivity::class.java)
+//                startActivity(scannerIntent)
+//            }
+//        }
+//    }
 
-        if (requestCode == 12){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                scannerIntent = Intent(this, ScannerActivity::class.java)
-                startActivity(scannerIntent)
+    private fun registerPermissionListener(){
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            if(it){
+                Toast.makeText(this, "Camera run", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
             }
         }
     }
