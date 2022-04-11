a1c58105b5a8af22f7c31b4e3b24964b7db64ed7
Dmitriy <dimryb@bk.ru>
2022-04-12 00:04:28 +0700

    Работающий уровень

--- app/src/main/java/space/rybakov/qr/MainActivity.kt
+++ app/src/main/java/space/rybakov/qr/MainActivity.kt
@@ -59,7 +59,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         sManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        //registerSensorAccelerometer()
+
         registerSensors()
     }
 
@@ -67,12 +67,43 @@ class MainActivity : AppCompatActivity() {
         val tvAngle = findViewById<TextView>(R.id.tvAngle)
         val ivLevel = findViewById<ImageView>(R.id.ivLevel)
         val tvSensor = findViewById<TextView>(R.id.tvSensor)
-        sensorRotation = sManager.getDefaultSensor(TYPE_ACCELEROMETER)
+        sensorRotation = sManager.getDefaultSensor(TYPE_ROTATION_VECTOR)
+        sensorAccelerometer = sManager.getDefaultSensor(TYPE_ACCELEROMETER)
         sv = object : SensorEventListener{
             override fun onSensorChanged(sEvent: SensorEvent?) {
-                val value = sEvent?.values
-                val sData = "nX: ${value?.get(0)}\nY: ${value?.get(1)}\nZ: ${value?.get(2)}"
-                tvSensor.text = sData
+                when{
+                    sEvent?.sensor == sensorRotation -> {
+                        val rotationMatrix = FloatArray(16)
+                        SensorManager.getRotationMatrixFromVector(rotationMatrix, sEvent?.values)
+                        val remappedRotationMatrix = FloatArray(16)
+                        SensorManager.remapCoordinateSystem(rotationMatrix,
+                            SensorManager.AXIS_X,
+                            SensorManager.AXIS_Z,
+                            remappedRotationMatrix
+                        )
+                        val orientations = FloatArray(16)
+                        SensorManager.getOrientation(remappedRotationMatrix, orientations)
+                        for(i in 0..3 ){
+                            orientations[i] = Math.toDegrees(orientations[i].toDouble()).toFloat()
+                        }
+
+                        val sData = "X: " + String.format("%.1f°", orientations[0]) + "\n" +
+                                "Y: " + String.format("%.1f°", orientations[1]) + "\n" +
+                                "Z: " + String.format("%.1f°", orientations[2]) + "\n"
+                        tvAngle.text = sData
+                        ivLevel.rotation = -orientations[2]
+                    }
+                    sEvent?.sensor == sensorAccelerometer -> {
+                        val gravitationalAcceleration = 9.81
+                        val accelerationX = sEvent?.values?.get(0)?.div(gravitationalAcceleration)
+                        val accelerationY = sEvent?.values?.get(1)?.div(gravitationalAcceleration)
+                        val accelerationZ = sEvent?.values?.get(2)?.div(gravitationalAcceleration)
+                        val sData = "X: " + String.format("%.2fg", accelerationX) + "\n" +
+                                "Y: " + String.format("%.2fg", accelerationY) + "\n" +
+                                "Z: " + String.format("%.2fg", accelerationZ) + "\n"
+                        tvSensor.text = sData
+                    }
+                }
             }
 
             override fun onAccuracyChanged(sensor: Sensor?, p1: Int) {
@@ -85,28 +116,12 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         sManager.registerListener(sv, sensorRotation, SensorManager.SENSOR_DELAY_FASTEST)
+        sManager.registerListener(sv, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
         sManager.unregisterListener(sv)
-    }
-
-    private fun registerSensorAccelerometer(){
-        val tvSensor = findViewById<TextView>(R.id.tvSensor)
-        sensorAccelerometer = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        val sListener = object : SensorEventListener{
-            override fun onSensorChanged(sEvent: SensorEvent?) {
-                val value = sEvent?.values
-                val sData = "nX: ${value?.get(0)}\nY: ${value?.get(1)}\nZ: ${value?.get(2)}"
-                tvSensor.text = sData
-            }
-
-            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-                TODO("Not yet implemented")
-            }
-        }
-        sManager.registerListener(sListener, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     private fun generateQrCode(text: String){
