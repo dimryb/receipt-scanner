23967882941c105444f46d70aa5bd959ed280106
dmitry <dimryb@bk.ru>
2022-11-05 17:59:58 +0700

    Цвет ссылеи из ресурсов

--- app/src/main/java/space/rybakov/qr/presentation/ScannerResultFragment.kt
+++ app/src/main/java/space/rybakov/qr/presentation/ScannerResultFragment.kt
@@ -1,7 +1,6 @@
 package space.rybakov.qr.presentation
 
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,6 +11,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import space.rybakov.qr.R
 import space.rybakov.qr.databinding.FragmentScannerResultBinding
 
 class ScannerResultFragment : Fragment() {
@@ -33,11 +33,11 @@ class ScannerResultFragment : Fragment() {
         return binding.root
     }
 
-    private fun resultView(){
-        val textViewResult : TextView = binding.tvScannerResult
-        if (URLUtil.isNetworkUrl(args.text)){
+    private fun resultView() {
+        val textViewResult: TextView = binding.tvScannerResult
+        if (URLUtil.isNetworkUrl(args.text)) {
             // Todo: переделать на material design
-            textViewResult.setTextColor(Color.BLUE)
+            textViewResult.setTextColor(resources.getColor(R.color.purple_500, null))
             textViewResult.setOnClickListener {
                 Toast.makeText(context, "Click: ${args.text}", Toast.LENGTH_SHORT).show()
                 startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(args.text)))
